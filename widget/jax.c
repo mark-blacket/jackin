67e2f9068708309d3e19dfeff527c18d18438651
mark-blacket <51699458+mark-blacket@users.noreply.github.com>
2024-02-13 16:59:47 +0300

    added client checks

--- widget/jax.c
+++ widget/jax.c
@@ -5,7 +5,7 @@
 #include <jack/jack.h>
 #include <jack/statistics.h>
 
-static jack_client_t * client;
+static jack_client_t * client = NULL;
 static unsigned long xrun_counter;
 static float xrun_delay_us;
 
@@ -18,6 +18,7 @@ static int cb_xrun(void *arg)
 
 static PyObject * client_init(PyObject * self, PyObject * args)
 {
+    if (client) Py_RETURN_NONE;
     client = jack_client_open("jackin' client", JackNoStartServer, 0);
     if (!client) return PyErr_Format(PyExc_Exception, "Cannot initialize client");
     jack_set_xrun_callback(client, cb_xrun, NULL);
@@ -27,24 +28,30 @@ static PyObject * client_init(PyObject * self, PyObject * args)
 
 static PyObject * client_close(PyObject * self, PyObject * args) 
 {
-    jack_deactivate(client);
-    jack_client_close(client);
+    if (client) {
+        jack_deactivate(client);
+        jack_client_close(client);
+        client = NULL;
+    }
     Py_RETURN_NONE;
 }
 
 static PyObject * cpu_stats(PyObject * self, PyObject * args)
 {
+    if (!client) return PyErr_Format(PyExc_Exception, "Client not initialized");
     return Py_BuildValue("f", jack_cpu_load(client));
 }
 
 static PyObject * xrun_stats(PyObject * self, PyObject * args)
 {
+    if (!client) return PyErr_Format(PyExc_Exception, "Client not initialized");
     float max_delay = jack_get_max_delayed_usecs(client);
     return Py_BuildValue("(kff)", xrun_counter, xrun_delay_us, max_delay);
 }
 
 static PyObject * xrun_reset(PyObject * self, PyObject * args)
 {
+    if (!client) return PyErr_Format(PyExc_Exception, "Client not initialized");
     xrun_counter = 0;
     xrun_delay_us = 0;
     jack_reset_max_delayed_usecs(client);
@@ -57,11 +64,12 @@ static PyMethodDef methods[] = {
     {"cpu_stats",    cpu_stats,    METH_NOARGS, "Get JACK's repoerted CPU usage"},
     {"xrun_stats",   xrun_stats,   METH_NOARGS, "Get a 3-element tuple with xrun count, latest xrun delay and max xrun delay"},
     {"xrun_reset",   xrun_reset,   METH_NOARGS, "Reset xrun statistics"},
-    {0, 0, 0, 0}
+    {NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef module = { 
-    PyModuleDef_HEAD_INIT, "jax", "Basic xrun monitor for JACK", -1, methods, 0, 0, 0, 0
+    PyModuleDef_HEAD_INIT, "jax", "Basic xrun monitor for JACK", -1, 
+    methods, NULL, NULL, NULL, NULL
 };
 
 PyMODINIT_FUNC PyInit_jax(void)
--- widget/qtile_jack/inner.py
+++ widget/qtile_jack/inner.py
@@ -1,15 +1,11 @@
 import libqtile.widget.base as base
 from .jax import *
 
-_client_started = False
-
 class _JackWidget(base.ThreadPoolText):
     defaults = [("update_interval", 2, "Update interval in seconds")]
 
     def __init__(self, conns_file, **cfg):
-        if not _client_started:
-            client_init()
-            _client_started = True
+        client_init()
         super().__init__("", **cfg)
 
 class XrunWidget(_JackWidget):
